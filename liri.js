10c4064689d2ac1a965656eca98c7a71a4c27269
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-08-29 16:56:44 -0700

    spotify functionality

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var accessTokenKey = keysINeed.twitterKeys.access_token_key;
 var accessTokenSecret = keysINeed.twitterKeys.access_token_secret;
 
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 function pullTweets(tweets) {
 	var client = new Twitter({
@@ -23,6 +24,34 @@ function pullTweets(tweets) {
     }
 	});
 }
+function pullSpotify() {
+	var spotify = new Spotify({
+  	id: "e2b61f19a9754d838b39511d43fc5827",
+  	secret: "9c2fe91146254004b405ec9f5d3f3efd"
+	});
+ 	var search = process.argv[3];
+	spotify.search({ type: 'track', query: search }, function(err, data) {
+  	if (err) {
+    	return console.log('Error occurred: ' + err);
+ 		}
+ 		for (var i = 0; i < 1; i++) {
+			console.log(data.tracks.items[i].album.artists[i].name);
+			console.log(data.tracks.items[i].name);
+			console.log(data.tracks.items[i].preview_url);
+			console.log(data.tracks.items[i].album.name);
+    } 
+	});
+};
+
 if (process.argv[2]==="my-tweets") {
+	pullTweets();
+}
+if (process.argv[2]==="spotify-this-song") {
+	pullSpotify();
+}
+if (process.argv[2]==="movie-this") {
+	pullTweets();
+}
+if (process.argv[2]==="do-what-it-says") {
 	pullTweets();
 }
