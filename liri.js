97ae3cf539081d313711a6957a00edab92a9072a
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-08-29 13:35:29 -0700

    for loop functionality and command implementation

--- liri.js
+++ liri.js
@@ -5,22 +5,24 @@ var accessTokenKey = keysINeed.twitterKeys.access_token_key;
 var accessTokenSecret = keysINeed.twitterKeys.access_token_secret;
 
 var Twitter = require('twitter');
-var client = new Twitter({
-  consumer_key: consumerKey,
-  consumer_secret: consumerSecret,
-  access_token_key: accessTokenKey,
-  access_token_secret: accessTokenSecret
-});
- 
-var params = {screen_name: 'djcbootcamp'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  	data = JSON.parse(response.body);
-    console.log(data);
-  }
-});
-console.log(consumerKey)
-console.log(consumerSecret)
-console.log(accessTokenKey)
-console.log(accessTokenSecret)
- 
+
+function pullTweets(tweets) {
+	var client = new Twitter({
+  	consumer_key: consumerKey,
+  	consumer_secret: consumerSecret,
+  	access_token_key: accessTokenKey,
+  	access_token_secret: accessTokenSecret
+	});
+ 	var params = {screen_name: 'djcbootcamp'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  	if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log(tweets[i].text);
+      }
+    }
+	});
+}
+if (process.argv[2]==="my-tweets") {
+	pullTweets();
+}
